dcc58da4b10eaf49a347e1c8cc05ab8c9021e990
bronzdoc <lsagastume1990@gmail.com>
2016-05-12 22:46:21 -0600

    Adding SystemFacts#get_boot_id

--- lib/termy/system_facts.rb
+++ lib/termy/system_facts.rb
@@ -1,14 +1,21 @@
 require "time"
 
 class Termy::SystemFacts
-  attr_reader :date
+  attr_reader :date, :boot_id
 
   def initialize
     @date = get_date
+    @boot_id = get_boot_id("/proc/sys/kernel/random/boot_id")
   end
 
-  private
   def get_date
     Time.now
+  end
+
+  def get_boot_id(file)
+    file = File.open(file)
+    data = file.read
+    file.close
+    data
   end
 end
--- spec/lib/termy/system_facts_spec.rb
+++ spec/lib/termy/system_facts_spec.rb
@@ -16,4 +16,13 @@ RSpec.describe Termy::SystemFacts do |config|
       expect(@sys_facts.date).to eq("2016-05-12 19:39:15 -0600")
     end
   end
+
+  describe "#boot_id" do
+    it "should return the boot id" do
+      boot_id_file = File.new("/tmp/boot_id_file", "w")
+      boot_id_file.write("0992ad15-5af9-49b8-a258-f45dea895414")
+      boot_id_file.close
+      expect(@sys_facts.get_boot_id("/tmp/boot_id_file")).to eq("0992ad15-5af9-49b8-a258-f45dea895414")
+    end
+  end
 end
